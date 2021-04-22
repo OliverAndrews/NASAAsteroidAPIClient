29ce777f80860f22aac8c713e9d21b8faefb8ddf
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-04-22 11:55:36 -0400

    Added the retry policy

--- source/fallback/implementations/PollyJSRetryPolicy.ts
+++ source/fallback/implementations/PollyJSRetryPolicy.ts
@@ -1,10 +1,34 @@
 import { ResponseObject } from "../../client/models/ResponseObject";
 import { IRetryPolicy } from "../abstractions/IRetryPolicy";
+import * as polly from 'polly-js';
+import axios from "axios";
+import { HTTPResponse } from "../../resources/enums/HTTPResponse";
 
 export class PollyJSRetryPolicy implements IRetryPolicy
 {
+    private _polly: polly.Polly;
+    private _delay: number;
+    private _count: number;
+
+    constructor(retryCount: number, retryDelay: number) 
+    {
+        this._polly = polly();
+        this._delay = retryDelay;
+        this._count = retryCount;
+    }
+
     public async getWithPolicy(requestString: string): Promise<ResponseObject> {
-        throw new Error("Method not implemented.");
+        var result = await this._polly
+            .retry(this._count)
+            .executeForPromise(() => {
+                return axios.get(requestString);
+            })
+            .then((result) => {
+                return result;
+            }, (err) => {
+                return err;
+            });
+        return new ResponseObject(result.data, parseInt(result.status));
     }
 
 }
